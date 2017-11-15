89cc0290673f7f0cba608dbe3ce592490abf789e
MPadalko <mpadalko@mpadalko-mac.kiev.luxoft.com>
2017-11-15 12:29:42 +0200

    added sass-loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
@@ -19,3 +19,4 @@ class App extends Component {
 }
 
 export default App;
+
