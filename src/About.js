733f4bfc433d0bc309db8fb7e565a5ca6a5df3be
MPadalko <mpadalko@mpadalko-mac.kiev.luxoft.com>
2017-11-16 16:25:41 +0200

    fix about

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ class About extends Component {
               <h3>Goals</h3>
               <p>Lorem ipsum dolor sit amet, ac erat dolor quisque magna consectetur leo, pede nec phasellus praesent adipiscing. Nullam diam magna at mauris amet, non ut urna a commodo. Donec vivamus semper nisi velit, proin id turpis, vehicula amet nullam convallis adipiscing ut sit, sit viverra. Duis interdum eu eu sodales nec ut, non nibh accumsan eleifend. Purus ut vitae sapien dui duis dolores, porttitor tempus nascetur id ut, integer ut dignissim harum. </p>
             </div>
-            <div className="icon-user-circle-o col-md-"></div>
+            <div className="icon-user-circle-o col-md-4"></div>
             <div className="col-md-4">
               <h3>Dreams</h3>
               <p>Lorem ipsum dolor sit amet, ac erat dolor quisque magna consectetur leo, pede nec phasellus praesent adipiscing. Nullam diam magna at mauris amet, non ut urna a commodo. Donec vivamus semper nisi velit, proin id turpis, vehicula amet nullam convallis adipiscing ut sit, sit viverra. Duis interdum eu eu sodales nec ut, non nibh accumsan eleifend. Purus ut vitae sapien dui duis dolores, porttitor tempus nascetur id ut, integer ut dignissim harum. </p>
